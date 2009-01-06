9ef7339369da44a434b479b2782fe616f7cfcfe7
Matt Kemp <matt@mattikus.com>
2009-01-06 10:38:48 -0600

    initial commit

---
+++ pygist.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+"""
+Python command line client for gist.github.com
+
+Based on Chris Wanstrath's ruby gist client: 
+    http://github.com/defunkt/gist/tree/master
+    
+Usage:
+    cat file.txt | pygist
+    pygist file1 file2 file3 file4
+    pygist -g 1234 > something.txt
+
+"""
+__author__ = 'Matt Kemp <matt@mattikus.com>'
+__version__ = 'pygist 0.1 1/6/09'
+__license__ = """
+Copyright (c) 2008 Matt Kemp <matt@mattikus.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os
+import sys
+import urllib2
+
+from urllib import urlencode as urlencode
+
+site = 'http://gist.github.com/gists'
+
+def gen_req(files):
+    i = 1
+    data = []
+
+    for filename in files:
+        if filename is sys.stdin:
+            ext = '.txt'
+        else:
+            ext = os.path.splitext(filename)[1]
+
+        contents = open(filename).read()
+        fname = os.path.basename(filename)
+
+        tmp = {
+            'file_ext[gistfile%d]' % i: ext,
+            'file_name[gistfile%d]' % i: fname,
+            'file_contents[gistfile%d]' % i: contents,
+        }
+        data.append(urlencode(tmp))
+        i += 1
+
+    return ''.join(data)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser(version=__version__)
+    parser.set_usage("%prog [options] [file1 file2 ...]")
+    parser.set_description(__doc__)
+
+    opts, args = parser.parse_args()
+
+    # Print message with no arguments so users don't think its hung
+    if os.isatty(sys.stdin.fileno()) and not args and not opts.getlang:
+        parser.print_help()
+        sys.exit()
+
+    data = gen_req(args)
+    info = urllib2.urlopen(site, data)
+    print info.geturl()
